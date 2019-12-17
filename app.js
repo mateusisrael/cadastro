d9b342433a5bb54715248a9b36d89f53f36be789
mateusisrael <mateuss.i.fernandes@gmail.com>
2019-12-16 23:15:38 -0300

    Delete

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ app.set('view engine', 'ejs');
 
 
 
-app.get('/', (req, res, next) => {
-    res.render('index.ejs');
-    next();
-});
+app.route('/')
+    .get((req, res, next) => {
+        res.render('home.ejs');
+        next();
+    });
 
 // app.get('/users', (req, res, next) => {
 //     res.send(users_data_json.users);
@@ -35,25 +36,27 @@ app.get('/', (req, res, next) => {
 //     lista.push(res);
 // }
 
-app.get('/cadastro', (req, res, next) => {
-    let cursor = cadastros.find().toArray((err, result) => {
-        if(err) return console.log(err);
+app.route('/cadastro')
+    .get((req, res, next) => {
+        let cursor = cadastros.find().toArray((err, result) => {
+            if(err) return console.log(err);
 
-        res.render('cadastro.ejs', {lista: result});
+            res.render('cadastro.ejs', {lista: result});
+        });
     });
 
+
+app.route('/cadastrar')
+    .post((req, res, next) => {
+
+        cadastros.save(req.body, (err, result) => {
+            if(err) return console.log(err);
+            console.log(`Salvo no banco de dados! ${result}`);
+            res.redirect('/cadastro');
+        });
+        // guardarDados(req.body);
+    });
     
-});
-
-app.post('/cadastrar', (req, res, next) => {
-
-    cadastros.save(req.body, (err, result) => {
-        if(err) return console.log(err);
-        console.log(`Salvo no banco de dados! ${result}`);
-        res.redirect('/cadastro');
-    });
-    // guardarDados(req.body);
-});
 
 app.route('/edit/:name')
     .get((req, res) => {
@@ -65,20 +68,41 @@ app.route('/edit/:name')
         });
     })
 
-const mongo_login = { user:"cadastrosdb2", password:"V3rTsptdVtEhkAV3" };
-const uri = `mongodb+srv://${mongo_login.user}:${mongo_login.password}@cluster0-pix3k.mongodb.net/test?retryWrites=true&w=majority`;
 
-MongoClient.connect(uri, (err, client) => {
-    if(err) return console.log(err);
+app.route('/delete/:id')
+    .get((req, res) => {
+        let id = req.params.id;
 
+        console.log(`Delete: ${req.params.id}`)
 
-    db = client.db('CRUD');
-    cadastros = db.collection('cadastros');
-
-// Importante: Lembrar de permitir o acesso do ip ao servidor mogodb.
-
-    app.listen(3000, () => {
-        console.log('Server on http://localhost:3000');
+        db.collection('cadastros').deleteOne({_id: id}, (err, result) => {
+            if (err) return res.send(500, err)
+            console.log('Deletado do Banco de Dados!')
+            res.redirect('/cadastro')
+        }) ;
     });
 
-});
+
+
+function iniciar() {
+
+    const mongo_login = { user:"cadastrosdb2", password:"V3rTsptdVtEhkAV3" };
+    const uri = `mongodb+srv://${mongo_login.user}:${mongo_login.password}@cluster0-pix3k.mongodb.net/test?retryWrites=true&w=majority`;
+    
+    MongoClient.connect(uri, (err, client) => {
+        if(err) return console.log(err);
+    
+    
+        db = client.db('CRUD');
+        cadastros = db.collection('cadastros');
+    
+    // Importante: Lembrar de permitir o acesso do ip ao servidor mogodb.
+    
+        app.listen(3000, () => {
+            console.log('Server on http://localhost:3000');
+        });
+    
+    });
+}
+
+iniciar();
--- routes.js
+++
@@ -1,7 +0,0 @@
-const route = require('express').Router;
-
-
-route('/')
-    .get((req, res) => {
-        console.log('Rota');
-    });
